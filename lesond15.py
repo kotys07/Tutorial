367599cdf4f6534e605e30fb20103505f265ea39
Denis Kotys <kotisden@gmail.com>
2017-11-30 18:44:43 +0200

    comit

--- lesond15.py
+++
@@ -1,6 +0,0 @@
-with open('fl.txt', 'wt', encoding='utf-8') as inFile:
-	num = int(input('Введи число - '))
-	line = str('1/ ' + str(num) + ' = ' + str(1 / num))
-	print(line)
-	inFile.write (line)
-	
